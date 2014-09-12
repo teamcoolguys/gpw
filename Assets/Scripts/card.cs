61d72e55c00e7ac3c88dc51d5fd7dab2afeb8b8f
teamcoolguys <modoosi@gmail.com>
2014-09-11 18:24:30 -0700

    Networking is Go

---
+++ Assets/Scripts/card.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class card : MonoBehaviour 
+{
+	public int AttackModifier;
+	public int DefenceModifier;
+
+	public GameObject Card;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
